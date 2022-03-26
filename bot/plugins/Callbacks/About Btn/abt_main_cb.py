95f92c82d7f9b6432b831e70d53b0bcefa3b5c0f
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2022-03-26 08:15:13 +0530

    Update abt_main_cb.py

--- bot/plugins/Callbacks/About Btn/abt_main_cb.py
+++ bot/plugins/Callbacks/About Btn/abt_main_cb.py
@@ -32,14 +32,14 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2">Source</a>"""
+    text+=f"""\n<b><i>Bot :</i></b> <a href="https://t.me/arshad6153">@arshad6153</a>\n"""
+    text+="""\n<b><i>🙌Source Code🙌:</i></b> <a href="https://t.me/+wzVkjXy58A5lOTFl">Source</a>"""
 
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "My Dev ⚡", url="//t.me/arshad6153"
                 ),
                 
             InlineKeyboardButton
